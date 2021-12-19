7a409a8d78170dca2aaccebecb609db533a0b621
sam bacha <sam@manifoldfinance.com>
2021-12-19 04:33:56 -0800

    build(nextjs): outDir

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
-  distDir: 'out',
+//  distDir: 'out',
   productionBrowserSourceMaps: false,
   poweredByHeader: false,
   reactStrictMode: true,
